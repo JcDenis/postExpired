16ffcf67d38e397b477f3b726b07566daa0af884
JcDenis <contact@jcdenis.fr>
2021-08-20 21:07:26 +0200

    Init

---
+++ _define.php
@@ -0,0 +1,34 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# This file is part of postExpired, a plugin for Dotclear 2.
+# 
+# Copyright (c) 2009-2013 Jean-Christian Denis and contributors
+# contact@jcdenis.fr http://jcd.lv
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')){return;}
+ 
+$this->registerModule(
+	/* Name */
+	"Expired entries",
+	/* Description*/
+	"Change entries options at a given date",
+	/* Author */
+	"Jean-Christian Denis",
+	/* Version */
+	'2013.11.13',
+	/* Properies */
+	array(
+		'permissions' => 'usage,contentadmin',
+		'type' => 'plugin',
+		'dc_min' => '2.6',
+		'support' => 'http://jcd.lv/q=postExpired',
+		'details' => 'http://plugins.dotaddict.org/dc2/details/postExpired'
+	)
+);
---
+++ locales/fr/main.lang.php
@@ -0,0 +1,62 @@
+<?php
+// Language: Français 
+// Module: postExpired - 2013.11.03
+// Date: 2013-11-03 22:12:31 
+// Translated with dcTranslater - 2013.05.11 
+
+#_admin.php:100
+#_admin.php:110
+$GLOBALS['__l10n']['Expired entries'] = 'Billets périmés';
+
+#_admin.php:101
+$GLOBALS['__l10n']['Add expired date'] = 'Ajouter une date de péremption';
+
+#_admin.php:111
+$GLOBALS['__l10n']['Remove expired date'] = 'Retirer une date de péremption';
+
+#_admin.php:139
+$GLOBALS['__l10n']['Expired date'] = 'Date de péremption';
+
+#_admin.php:209
+$GLOBALS['__l10n']['Expired date added.'] = 'Date de péremption ajoutée.';
+
+#_admin.php:222
+$GLOBALS['__l10n']['Add expired date to this selection'] = 'Ajouter une date de péremtion à cette sélection';
+
+#_admin.php:269
+$GLOBALS['__l10n']['Expired date deleted.'] = 'Dtae de péremtion supprimé';
+
+#_admin.php:370
+$GLOBALS['__l10n']['On this date, change:'] = 'Á cette date, changer :';
+
+#_admin.php:398
+$GLOBALS['__l10n']['Selection:'] = 'Séléction :';
+
+#_admin.php:409
+$GLOBALS['__l10n']['Comments status:'] = 'Status des commentaires :';
+
+#_admin.php:419
+$GLOBALS['__l10n']['Trackbacks status:'] = 'Status des rétroliens :';
+
+#_admin.php:440
+#_admin.php:470
+#_admin.php:485
+#_admin.php:499
+#_admin.php:513
+$GLOBALS['__l10n']['Not changed'] = 'Inchangé';
+
+#_admin.php:500
+#_admin.php:514
+$GLOBALS['__l10n']['Opened'] = 'Ouvert';
+
+#_admin.php:501
+#_admin.php:515
+$GLOBALS['__l10n']['Closed'] = 'Fermé';
+
+#_public.php:25
+$GLOBALS['__l10n']['Expired on'] = 'Expire le';
+
+#_public.php:26
+$GLOBALS['__l10n']['This entry has no expiration date'] = 'Ce billet n\'a pas de date de péremption';
+
+?>
