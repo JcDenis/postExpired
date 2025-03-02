d01f731de513537b70fc0340ec2c075573d95367
Jean-Christian Denis <contact@jcdenis.fr>
2025-03-02 18:45:37 +0100

    release 2025.03.02

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Expired entries',
     'Change entries options at a given date',
     'Jean-Christian Denis and Contributors',
-    '2023.11.04',
+    '2025.03.02',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
